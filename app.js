320db695bd7a5b704aeeb0e47e5eba5dde309155
NguyenThanh1995 <thanhnguyennguyen1995@gmail.com>
2021-04-06 14:15:51 +0700

    fix cross domain

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const app = require("express")();
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
-app.use(require("cors")());
+app.use(require("cors")({
+  origin: "*",
+  optionsSuccessStatus: 200
+}));
+app.use(require("cookie-parser")());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use("/", require("./routes"));
 
 app.listen(process.env.PORT || 3000, (err) => {
